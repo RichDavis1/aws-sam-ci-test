719940520a6ae8e139e94c80449020b1809c8c00
RichDavis1 <63970045+RichDavis1@users.noreply.github.com>
2020-09-27 17:41:07 -0500

    Update entrypoint.sh

--- .github/actions/sam/package/entrypoint.sh
+++ .github/actions/sam/package/entrypoint.sh
@@ -48,9 +48,9 @@ function runSam(){
 		echo "//npm.pkg.github.com/:_authToken=${INPUT_GITHUB_PACKAGE_REGISTRY_TOKEN}" > ~/.npmrc
 	fi
 	
-	echo "go path"
+	#echo "go path"
 	
-	echo "${GODEBUG}"
+	#echo "${GODEBUG}"
 	echo "Run sam ${INPUT_SAM_COMMAND}"
 	output=$(sam ${INPUT_SAM_COMMAND} 2>&1)
 	exitCode=${?}
