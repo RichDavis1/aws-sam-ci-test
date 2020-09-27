ce35116d4ee4a3967680ac0f038525b821325936
Rich Davis <living.tribunal5919@gmail.com>
2020-09-27 14:36:04 -0500

    Update main.go

--- sam-testaroni/hello-world/main.go
+++ sam-testaroni/hello-world/main.go
@@ -41,7 +41,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello ok test v4, %v", string(ip)),
+		Body:       fmt.Sprintf("Hello ok test v5, %v", string(ip)),
 		StatusCode: 200,
 	}, nil
 }
