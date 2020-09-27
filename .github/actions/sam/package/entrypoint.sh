1f4ca59606f244ec244f8468a426e27553e7ddb6
RichDavis1 <63970045+RichDavis1@users.noreply.github.com>
2020-09-27 16:24:19 -0500

    Update entrypoint.sh

--- .github/actions/sam/package/entrypoint.sh
+++ .github/actions/sam/package/entrypoint.sh
@@ -13,16 +13,16 @@ function parseInputs(){
 function installAwsSam(){
 	echo "Install aws-sam-cli ${INPUT_SAM_VERSION}"
 	if [ "${INPUT_SAM_VERSION}" == "latest" ]; then
-		pip install --user aws-sam-cli
+		pip3 install --user aws-sam-cli
 		#pip3 install aws-sam-cli >/dev/null 2>&1
 		if [ "${?}" -ne 0 ]; then
 			#test = pip3 install aws-sam-cli >/dev/null 2>&1
 			#echo "Run sam ${INPUT_SAM_COMMAND}"
-			output=$(pip install --user aws-sam-cli)
+			#output=$(pip install --user aws-sam-cli)
 			#output=$(pip3 install aws-sam-cli 2>&1)
 			#output=$(pip3 install aws-sam-cli >/dev/null 2>&1)			
-			echo "${output}"
-			echo "${?}"
+			#echo "${output}"
+			#echo "${?}"
 			echo "done printing errors"
 			#test = pip --version
 			#echo "${test}"
