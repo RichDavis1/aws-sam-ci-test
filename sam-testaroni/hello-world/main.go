2380680c8a9ed87fa60daf297d6f28549f3275e6
RichDavis1 <63970045+RichDavis1@users.noreply.github.com>
2020-09-29 09:53:22 -0500

    Update main.go

--- sam-testaroni/hello-world/main.go
+++ sam-testaroni/hello-world/main.go
@@ -41,7 +41,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello richie update, %v", string(ip)),
+		Body:       fmt.Sprintf("Hello richie update please, %v", string(ip)),
 		StatusCode: 200,
 	}, nil
 }
