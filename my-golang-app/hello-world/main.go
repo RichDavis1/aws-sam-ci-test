7dc8a03f8391540720923635255f15f23fbf9ccf
RichDavis1 <63970045+RichDavis1@users.noreply.github.com>
2020-10-05 11:34:08 -0500

    Update main.go

--- my-golang-app/hello-world/main.go
+++ my-golang-app/hello-world/main.go
@@ -41,7 +41,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello World V2"),
+		Body:       fmt.Sprintf("Hello World V3"),
 		StatusCode: 200,
 	}, nil
 }
