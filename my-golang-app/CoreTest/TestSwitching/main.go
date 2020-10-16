ad5b6d9070ab6708359e158c0aca589091aada7e
RichDavis1 <63970045+RichDavis1@users.noreply.github.com>
2020-10-16 14:11:55 -0500

    Update main.go

--- my-golang-app/CoreTest/TestSwitching/main.go
+++ my-golang-app/CoreTest/TestSwitching/main.go
@@ -48,7 +48,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	*/
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello World stage2"),
+		Body:       fmt.Sprintf("Hello World stage4"),
 		StatusCode: 200,
 	}, nil
 }
