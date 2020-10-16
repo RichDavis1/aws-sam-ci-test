b48f6eb9195dc64c8bf228462b390b20b8728e8f
Rich Davis <living.tribunal5919@gmail.com>
2020-10-16 17:19:50 -0500

    test
    test

--- my-golang-app/CoreTest/TestSwitching/main.go
+++ my-golang-app/CoreTest/TestSwitching/main.go
@@ -49,7 +49,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	fmt.Println("test")
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello World reverted"),
+		Body:       fmt.Sprintf("Hello World stage"),
 		StatusCode: 200,
 	}, nil
 }
