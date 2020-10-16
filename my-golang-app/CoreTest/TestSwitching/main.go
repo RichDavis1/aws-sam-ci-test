3c326add5be035740c87ffe61b0a5854f4e20ab5
Rich Davis <living.tribunal5919@gmail.com>
2020-10-16 15:59:48 -0500

    test
    test

--- my-golang-app/CoreTest/TestSwitching/main.go
+++ my-golang-app/CoreTest/TestSwitching/main.go
@@ -46,10 +46,8 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 				return events.APIGatewayProxyResponse{}, ErrNoIP
 			}
 	*/
-			
-	
+
 	return events.APIGatewayProxyResponse{
-		'
 		Body:       fmt.Sprintf("Hello World failed"),
 		StatusCode: 200,
 	}, nil
