56d5e5c4db4f6d57c411a759a338d4be3ab64ea3
Rich Davis <living.tribunal5919@gmail.com>
2020-10-19 10:46:46 -0500

    test
    test

--- my-golang-app/CoreTest/TestSwitching/main.go
+++ my-golang-app/CoreTest/TestSwitching/main.go
@@ -49,7 +49,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	fmt.Println("test")
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello World Full process for real"),
+		Body:       fmt.Sprintf("Hello World Full process for real, here we go"),
 		StatusCode: 200,
 	}, nil
 }
